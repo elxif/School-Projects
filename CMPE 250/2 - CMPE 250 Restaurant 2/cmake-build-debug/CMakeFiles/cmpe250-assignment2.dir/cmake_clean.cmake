67ba7edb375265ac499ec691c00a5ad16d11d9f2
Elif Akalın <elifnurakalin@gmail.com>
2021-04-01 14:00:58 +0300

    Add files via upload

---
+++ CMPE 250/2 - CMPE 250 Restaurant 2/cmake-build-debug/CMakeFiles/cmpe250-assignment2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmpe250-assignment2.dir/RestaurantOrganizer.cpp.o"
+  "CMakeFiles/cmpe250-assignment2.dir/main.cpp.o"
+  "cmpe250-assignment2"
+  "cmpe250-assignment2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmpe250-assignment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
